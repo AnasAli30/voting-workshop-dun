37e4abf28fa18400bdc5caf7bf2ca98513abd786
Anas Ali <anasali12665@gmail.com>
2025-02-26 19:46:53 +0000

    finall fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-      NEXT_HELIUS_RPC_URL: process.env.NEXT_HELIUS_RPC_URL,
-    },
+
     webpack: (config) => {
       config.externals.push('pino-pretty', 'lokijs', 'encoding');
       return config;
